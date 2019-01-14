3575f860275d46e78ff0623377c66a77332fa128
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-13 21:42:44 -0500

    Create json_practice_2.py

---
+++ json_practice_2.py
@@ -0,0 +1,14 @@
+import json
+
+json_practice = 'favorite_number.json'
+
+try:
+	with open(json_practice) as file_obj:
+		favorite_number = json.load(file_obj)
+except FileNotFoundError:
+	user_input = input('Enter your favorite number:')
+	with open(json_practice, 'w') as file_obj:
+		json.dump(user_input, file_obj)
+		print('your favorite number..' + favorite_number + ' has been dumped into a file for storage!!')
+else:
+	print('your favorite number is..' + favorite_number + '!' )
