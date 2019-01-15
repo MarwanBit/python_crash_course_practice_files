cf3bffd74a34ffb74c5c9ae5218a23b28d5fec4f
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 18:46:58 -0500

    Delete apostrophe.py

--- apostrophe.py
+++
@@ -1,7 +0,0 @@
-message = "One of Python's strengths is its diverse community."
-print(message)
-
-# below is an intentional error to show how syntax errors work
-
-message = 'One of Python's strengths is its diverse community.'
-print(message)
