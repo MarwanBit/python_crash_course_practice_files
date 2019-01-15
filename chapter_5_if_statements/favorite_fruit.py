64fcb6c20986394cfbb365c5d97762935a174a03
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 18:48:38 -0500

    Create favorite_fruit.py

---
+++ chapter_5_if_statements/favorite_fruit.py
@@ -0,0 +1,13 @@
+favorite_fruits = ['Strawberries', 'Watermelon', 'Tomatoes', 'Cherries'
+					, 'Apples', 'Pears',]
+					
+if 'Strawberries' in favorite_fruits:
+	print('Damn you must really like strawberries')
+if 'Pears' in favorite_fruits:
+	print('You like pears, I like peeaaaaars')
+if 'Apples' in favorite_fruits:
+	print('I love apples too, there a classic fruit')
+if 'Cherries' in favorite_fruits:
+	print('Cherries are not that good, there extremely overrated,')
+if 'Tomatoes' in favorite_fruits:
+	print('Really tomatoes, there not even a real fruit, you are joking right?')
