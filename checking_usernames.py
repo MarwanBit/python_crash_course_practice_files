e40b08b021e170535b7a7143aafad6ba3167d9fe
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 18:48:53 -0500

    Delete checking_usernames.py

--- checking_usernames.py
+++
@@ -1,9 +0,0 @@
-current_users = ['Logic', 'Denzel Curry', 'Kendrick Lamar', 'IDK', 
-			     'J. Cole', 'Lupe Fiasco', 'Joey Badass', 'Russ']
-new_users = ['YBN Cordae', 'BROCKHAMPTON', 'Bas', 'IDK', 'Russ']
-
-for username in new_users:
-	if username in current_users:
-		print('Sorry' + ' ' + str(username) + ' but that username has been taken, try another one')
-	elif username not in current_users:
-		print('The username' + ' ' + str(username) + ' is available')
