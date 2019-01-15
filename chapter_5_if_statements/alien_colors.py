37637a84c871b49710fd19a0abf89053ae9ff912
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 18:48:31 -0500

    Create alien_colors.py

---
+++ chapter_5_if_statements/alien_colors.py
@@ -0,0 +1,12 @@
+alien_color = 'Green'
+
+if alien_color == 'Green':
+	print('you just earned 5 points for shooting down a green alien!')
+
+print('\n\tnext alien...')
+
+alien_color = 'Red'
+if alien_color == 'Green':
+	print('you just earned 5 points for shooting down a green alien!')
+else:
+	print('Sorry you did not shoot down a green alien, no points for you')
