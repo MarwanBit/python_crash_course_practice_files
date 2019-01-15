c8c992d51ce9ea72c0742206c3e6defff36c1a8f
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 18:48:19 -0500

    Create intro.py

---
+++ chapter_2_variables_and_simple_data_types/intro.py
@@ -0,0 +1,16 @@
+Zacharia = "Zacharia is ass at life"
+
+print(Zacharia)
+print("Zacharia")
+
+zacharias_friends = ["Bob", "Jerry", "Samuel", "Timmy"]
+
+print(zacharias_friends[0])
+
+counter = 0
+
+while len(zacharias_friends) >= 1:
+	print(zacahrias_friends[counter])
+	counter += 1 
+	if counter == 3:
+		zacharias_friends = 0 
