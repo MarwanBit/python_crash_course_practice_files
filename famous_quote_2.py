be3b6fc1b7502bc21cde0bd0e200bca7776fd955
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 18:49:22 -0500

    Delete famous_quote_2.py

--- famous_quote_2.py
+++
@@ -1,4 +0,0 @@
-name = "Sir Robert Bryson Hall II a.k.a Logic"
-famous_quote = ' How can the sky be the limit when theres footprints on the moon?'
-famous_quote =( name + " " + " once said," + '"' + famous_quote + '"')
-print(famous_quote)
