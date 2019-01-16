b580c31fd14dcc5e464910e553f7299dda0c84bb
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 19:06:51 -0500

    Delete simple_messages_2.py

--- simple_messages_2.py
+++
@@ -1,5 +0,0 @@
-woah_bro = "woah man i was only fucking your cat in the ass, he's still a virgin so its ok, your just jealous cause i creampied her"
-print(woah_bro)
-
-intro = "in the land of Herxulahh lies a hero by the name of Xerxes, he would rise to become the greatest philosopher of the technological middle ages"
-print(intro)
