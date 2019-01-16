c59675980261fcc1604bc8d3d69b2295e86a0ab8
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 19:05:01 -0500

    Delete lists_practice_personal.py

--- lists_practice_personal.py
+++
@@ -1,5 +0,0 @@
-teachers_to_greet = [ "Mr.Nazak", "Mr.Jackson", "Mr.McGinnis", "Mrs.Strong", "Mrs.Richardson", "Mrs.K Smith"]
-
-for i in teachers_to_greet:
-	message = "Hi" + " " + str(i) + "," + " " + " I appreciate your impact on my academic career"
-	print(message)
