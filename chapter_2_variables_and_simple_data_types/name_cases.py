299801635f32b1a8863ee88169b4961d3e4a0a0a
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 19:03:23 -0500

    Create name_cases.py

---
+++ chapter_2_variables_and_simple_data_types/name_cases.py
@@ -0,0 +1,12 @@
+name = "Hulia"
+name_2 = "Jason"
+name_3 = "Denzel"
+name_4 = "Pasckio"
+
+name_list = [name, name_2, name_3, name_4]
+
+for i in name_list:
+	print(i.upper())
+	print(i.lower())
+	print(i.title())
+	
