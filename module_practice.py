54c153d75e1fab7b4c252ada3a8521f4c33dd163
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 19:05:15 -0500

    Delete module_practice.py

--- module_practice.py
+++
@@ -1,8 +0,0 @@
-import function_practice
-from function_practice import greet_user
-from function_practice import greet_user, make_shirt
-from function_practice import greet_user as gu
-import function_practice as fp
-#The command below imports all functions from a module
-from function_practice import *
-
