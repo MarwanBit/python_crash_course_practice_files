6fa58f531cef64b642006d90a2d48d2126be66d1
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 19:06:32 -0500

    Delete simple_messages.py

--- simple_messages.py
+++
@@ -1,5 +0,0 @@
-message = "China is a rising nation with one of the most influential economies"
-print(message)
-
-message = "J. Cole went 2x Platinum on his 2014 album, 2014 Forest Hills Drive"
-print(message)
