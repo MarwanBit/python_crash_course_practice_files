8b0c68a18c26530b7a8c31b12d414e05622c4f6b
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 19:03:54 -0500

    Create lists_practice_using_friends.py

---
+++ chapter_4_working_with_lists/lists_practice_using_friends.py
@@ -0,0 +1,7 @@
+friends_names = [ "Bryan", "Zain", "Amine", "Owen", "Landon", "Love"]
+
+for i in friends_names:
+	message = "Salutations," + " " + str(i) + ", " + "my friend"
+	print(message)
+	
+	
