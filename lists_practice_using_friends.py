b202e7bb4277d362934ccb36bc0cbe18da40882c
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-15 19:05:04 -0500

    Delete lists_practice_using_friends.py

--- lists_practice_using_friends.py
+++
@@ -1,7 +0,0 @@
-friends_names = [ "Bryan", "Zain", "Amine", "Owen", "Landon", "Love"]
-
-for i in friends_names:
-	message = "Salutations," + " " + str(i) + ", " + "my friend"
-	print(message)
-	
-	
