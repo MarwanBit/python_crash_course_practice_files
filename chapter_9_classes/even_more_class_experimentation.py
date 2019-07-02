d77030152e68d10238fddb82add3156151149e9a
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-07-02 13:01:11 -0400

    Update even_more_class_experimentation.py

--- chapter_9_classes/even_more_class_experimentation.py
+++ chapter_9_classes/even_more_class_experimentation.py
@@ -7,11 +7,13 @@ class TkinterInheritance(tk.Tk):
 
 		tk.Tk.__init__(self, *args)
 
-		self.container = tk.Frame(self)
+		self.container = tk.Frame(self, height=300, width=300)
 		self.container.pack()
+
+		
 
 
 App = TkinterInheritance()
-print(App.title)
-print(App.container)
+canvas = tk.Entry(App.container)
+canvas.place(relx=0.5,rely=0.5, relheight=0.1, relwidth=0.4, anchor='n')
 App.mainloop()
