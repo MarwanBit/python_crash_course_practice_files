956938e1ac5ee9d7afe05a3afba80404650e7500
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-07-02 12:51:55 -0400

    Update even_more_class_experimentation.py

--- chapter_9_classes/even_more_class_experimentation.py
+++ chapter_9_classes/even_more_class_experimentation.py
@@ -7,6 +7,11 @@ class TkinterInheritance(tk.Tk):
 
 		tk.Tk.__init__(self, *args)
 
+		self.container = tk.Frame(self)
+		self.container.pack()
+
 
 App = TkinterInheritance()
 print(App.title)
+print(App.container)
+App.mainloop()
