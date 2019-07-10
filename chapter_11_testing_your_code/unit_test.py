c75cb042a5f19b42db9a2873cf33d0dae786000c
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-07-09 20:39:03 -0400

    Update unit_test.py

--- chapter_11_testing_your_code/unit_test.py
+++ chapter_11_testing_your_code/unit_test.py
@@ -1,12 +1,11 @@
 import unittest
-from get_name import get_name
+from testing import get_formatted_name
 
 class GetNameTestCase(unittest.TestCase):
 
 	def test_first_last_name(self):
-		combined_name = get_name('marwan', 'bit')
+		combined_name = get_formatted_name('marwan', 'bit')
 		self.assertEqual(combined_name, 'Marwan Bit')
 
 
-get_name('marwan', 'bit')
 unittest.main()
