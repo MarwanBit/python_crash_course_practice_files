646cb48313ac7cebda5fffa6e12c12287b912b44
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-07-09 20:38:56 -0400

    Create testing.py

---
+++ chapter_11_testing_your_code/testing.py
@@ -0,0 +1,6 @@
+
+def get_formatted_name(first, middle, last):
+	'''this function takes in a first and last name input and returns a formatted name'''
+	formatted_name = first + ' ' + middle + ' ' + last
+	return formatted_name.title()
+
