b4d193ab8a1b102efa1c1890140c8ca2be03436f
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-07-11 07:28:15 -0400

    Update videogame_project_2.py

--- pygame_experimentation/videogame_project_2.py
+++ pygame_experimentation/videogame_project_2.py
@@ -1,2 +1,20 @@
-import arcade 
+import pygame
 
+class Screen(pygame.Surface):
+
+	def __init__(self):
+		'''Initializes the screen with a size of 400, 600'''
+		self.width = 600
+		self.height = 400
+		#Initializes the parent class Surface object
+		super().__init__(size =(self.width,self.height))
+
+		
+
+
+if __name__ == "__main__":
+	'''Runs the code below if the file is being executed directly'''
+	pygame.init()
+	window = Screen()
+	print(window)
+	pygame.display.set_mode(self.width, self.height)
