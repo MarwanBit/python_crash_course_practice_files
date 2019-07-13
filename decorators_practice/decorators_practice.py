80ddd69282ae1f262c938b170bd5d4dde4d0d00d
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-07-12 21:20:54 -0400

    Update decorators_practice.py

--- decorators_practice/decorators_practice.py
+++ decorators_practice/decorators_practice.py
@@ -22,9 +22,28 @@ print('add_one(5): ' + str(add_one(5)))
 #Part Two: Defining Functions inside Functions
 
 def plus_two(number):
+	'''plus_two passes number to add_one'''
 	def add_one(number):
 		return number + 1
+	#add_one is defined and then used inside plus_two
+	#twice, getting the effect of adding twice
 	result = add_one(add_one(number))
+	#this result is then returned when plus_two is called
 	return result 
 
 print('\nplus_two(4): ' + str(plus_two(4)))
+
+#//////////////////////////////////////////////////////// 
+
+#Part Three: Passing Functions as Arguments to other Functions
+
+def square_number(number):
+	'''squares the number inputed'''
+	return number * number 
+
+def subtract_1_
+
+# https://www.datacamp.com/community/tutorials/decorators-python 
+
+
+
